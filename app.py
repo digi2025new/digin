96f4c00c417950abae1f33229da4eafde29e6c7a
suraj <surajgolambade0dbit@gmail.com>
2025-03-24 11:30:07 +0530

    Fix port binding and update app.py for Render

--- app.py
+++ app.py
@@ -190,4 +190,5 @@ def slideshow(dept):
     return render_template('slideshow.html', department=dept, notices=notices, timer=timer)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
